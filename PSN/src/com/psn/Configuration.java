d37520dc4abaf34a14577c7077571ea7f2f79eb0
Fabio Santambrogio <Fabio Santambrogio@DESKTOP-UFS07CR>
2018-11-29 17:10:32 +0100

    Add constant for index on reading files

--- PSN/src/com/psn/Configuration.java
+++ PSN/src/com/psn/Configuration.java
@@ -1,14 +1,18 @@
 package com.psn;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public interface Configuration {
 
     public static final String APPLICATION_NAME = "PSN";
 
     public static final String VERSION = "1.0.0";
 
-    public static final String VENDOR_ID = "054c";
+    public static final String VENDOR_ID = "1234";
 
-    public static final String PRODUCT_ID = "02a5";
+    public static final String PRODUCT_ID = "ffff";
 
     public static final String FILE_ENCODED_EXT = "encx";
 
@@ -27,5 +31,7 @@ public interface Configuration {
     public static final String UI_ENCODE_BUTTON = "Codifica";
 
     public static final String UI_DECODE_BUTTON = "Decodifica";
+    
+    public static final Set<Integer> INDEX_SET = new HashSet<>(Arrays.asList(1, 3, 5, 7, 9));
 
 }
