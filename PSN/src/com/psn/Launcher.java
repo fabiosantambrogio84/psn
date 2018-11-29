c16181e8b4bac2f5d84c42d945ec8984569aa048
Fabio Santambrogio <Fabio Santambrogio@DESKTOP-UFS07CR>
2018-11-29 17:28:21 +0100

    Removed unused import and add System.exit(0)

--- PSN/src/com/psn/Launcher.java
+++ PSN/src/com/psn/Launcher.java
@@ -29,7 +29,6 @@ import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
