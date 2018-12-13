019c70d13ba5a79d2110c188eda06ba60b91685a
Fabio Santambrogio <Fabio Santambrogio@DESKTOP-UFS07CR.homenet.telecomitalia.it>
2018-12-13 11:58:09 +0100

    Add buffer size constant

--- PSN/src/com/psn/Configuration.java
+++ PSN/src/com/psn/Configuration.java
@@ -8,15 +8,13 @@ public interface Configuration {
 
     public static final String APPLICATION_NAME = "PSN";
 
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.1.0";
 
     public static final String VENDOR_ID = "1234";
 
     public static final String PRODUCT_ID = "ffff";
 
     public static final String FILE_ENCODED_EXT = "encx";
-
-    public static final String ALGORITHM = "5050";
 
     public static final String UI_STAGE_TITLE_LABEL = "File da processare";
 
@@ -33,5 +31,7 @@ public interface Configuration {
     public static final String UI_DECODE_BUTTON = "Decodifica";
     
     public static final Set<Integer> INDEX_SET = new HashSet<>(Arrays.asList(1, 3, 5, 7, 9));
+    
+    public static final int BUFFER_SIZE = 204800;
 
 }
